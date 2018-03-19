229482d6d2018b1fcde302752c2f5e4db7093687
elizabethfun@gmail.com <elizabethfun@gmail.com>
2018-03-19 18:19:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  return a-b;
+}
+
+function multiply(a, b){
+  return a*b
+}
+
+function divide(a, b){
+  return a/b
+}
+
+function inc(a){
+  return a+1
+}
+
+function dec(a){
+  return a-1
+}
+
+function makeInt(a){
+  return parseInt (a,'a')
+}
+
+function assumesBase10(){
+    return parseInt('0*2328')
+}
+
+function returnsNan(){
+  if (parseInt('0','10')) return 0}
+
+function preserveDecimal(){
+  return parseFloat('2.222')
+} 
+
+
+
+
+
+
+
